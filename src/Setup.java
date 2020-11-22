94638a783dbbe6226add202bec750e3a340ead26
Meme1sh <60296686+The-GitHub-Paradox@users.noreply.github.com>
2020-11-21 16:20:45 -0800

    e
    e

--- src/Setup.java
+++ src/Setup.java
@@ -1,74 +1,219 @@
-	import java.awt.Color;
-	import java.awt.event.ActionEvent;
-	import java.awt.event.ActionListener;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URL;
 
-	import javax.swing.JButton;
-	import javax.swing.JFrame;
-	import javax.swing.JPanel;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-	public class Setup implements ActionListener{
-		JButton b = new JButton();
-		JButton b1 = new JButton();
-		JButton b2 = new JButton();
-		JButton b3 = new JButton();
-		public static void main(String[] args) {
-			new Setup().run();
-		}
-		public void run() {
-			JFrame f = new JFrame("Simon Says");
-			f.setVisible(true);
-			JPanel p = new JPanel();
-			f.add(p);
-			f.setSize(500, 500);
-			b.setBackground(Color.yellow);
-			b.setOpaque(true);
-			b1.setBackground(Color.red);
-			b1.setOpaque(true);
-			b2.setBackground(Color.blue);
-			b2.setOpaque(true);
-			b3.setBackground(Color.green);
-			b3.setOpaque(true);
-			p.add(b);
-			p.add(b1);
-			p.add(b2);
-			p.add(b3);
-			b.addActionListener(this);
-			b1.addActionListener(this);
-			b2.addActionListener(this);
-			b3.addActionListener(this);
-			
-		}
-	    static void speak(String words) {
-	        if( System.getProperty( "os.name" ).contains( "Windows" ) ) {
-	            String cmd = "PowerShell -Command \"Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('"
-	                    + words + "');\"";
-	            try {
-	                Runtime.getRuntime().exec( cmd ).waitFor();
-	            } catch( Exception e ) {
-	                e.printStackTrace();
-	            }
-	        } else {
-	            try {
-	                Runtime.getRuntime().exec( "say " + words ).waitFor();
-	            } catch( Exception e ) {
-	                e.printStackTrace();
-	            }
-	        }
-	    }
-		@Override
-		public void actionPerformed(ActionEvent e) {
-	JButton buttonPressed = (JButton) e.getSource();
-		if(e.getSource() == b) {
-			speak("Yellow");
-			}
+import javazoom.jl.player.advanced.AdvancedPlayer;
+
+public class Setup implements ActionListener{
+	JFrame f;
+	JPanel p;
+	JButton b1;
+	JButton b2;
+	JButton b3;
+	JButton b4;
+	JButton b5;
+	JButton b6;
+	JButton b7;
+	JButton b8;
+	JButton b9;
+	public Song song;
+	public Song song1;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 500;
+	
+	Setup(){
+		song = new Song("E.mp3");
+		song1 = new Song("Swamp.mp3");
+		f = new JFrame();
+		p = new JPanel(new GridLayout());p.setPreferredSize(new Dimension(100, 100));
+		b1 = new JButton();
+		b2 = new JButton();
+		b3 = new JButton();
+		b4 = new JButton();
+		b5 = new JButton();
+		b6 = new JButton();
+		b7 = new JButton();
+		b8 = new JButton();
+		b9 = new JButton();
+		f.add(p);
+		p.add(b1);
+		p.add(b2);
+		p.add(b3);
+		p.add(b4);
+		p.add(b5);
+		p.add(b6);
+		p.add(b7);
+		p.add(b8);
+		p.add(b9);
+		b1.setBackground(Color.white);
+		b2.setBackground(Color.white);
+		b3.setBackground(Color.white);
+		b4.setBackground(Color.white);
+		b5.setBackground(Color.white);
+		b6.setBackground(Color.white);
+		b7.setBackground(Color.white);
+		b8.setBackground(Color.white);
+		b9.setBackground(Color.white);
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		b3.addActionListener(this);
+		b4.addActionListener(this);
+		b5.addActionListener(this);
+		b6.addActionListener(this);
+		b7.addActionListener(this);
+		b8.addActionListener(this);
+		b9.addActionListener(this);
+		b1.setText(" 1 ");
+		b2.setText(" 2 ");
+		b3.setText(" 3 ");
+		b4.setText(" 4 ");
+		b5.setText(" 5 ");
+		b6.setText(" 6 ");
+		b7.setText(" 7 ");
+		b8.setText(" 8 ");
+		b9.setText(" 9 ");
+	}
+
+	public void run() {
+		f.setSize(WIDTH, HEIGHT);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub	
+		JButton buttonPressed = (JButton) e.getSource();
 		if(e.getSource() == b1) {
-				speak("Red");
-			}
+			song.play();
+		}
 		if(e.getSource() == b2) {
-				speak("Blue");
-	}
+			song1.play();
+		}
 		if(e.getSource() == b3) {
-				speak("Green");
-	}
+			JOptionPane.showMessageDialog(null, "Ring a Ding.");
+		}
+		if(e.getSource() == b4) {
+			JOptionPane.showMessageDialog(null, "Ring a Ding!");
+		}
+		if(e.getSource() == b5) {
+			JOptionPane.showMessageDialog(null, "Ring a Ding Ding.");
+		}
+		if(e.getSource() == b6) {
+			JOptionPane.showMessageDialog(null, "Ring a Ding Ding!");
+		}
+		if(e.getSource() == b7) {
+			JOptionPane.showMessageDialog(null, "Ring a Ding Ding Ling.");
+		}
+		if(e.getSource() == b8) {
+			JOptionPane.showMessageDialog(null, "Ring a Ding Ding Ling!");
+		}
+		if(e.getSource() == b9) {
+			JOptionPane.showMessageDialog(null, "NULL");
 		}
 	}
+	private JLabel loadImage(String fileName) {
+		URL imageURL = getClass().getResource(fileName);
+		Icon icon = new ImageIcon(imageURL);
+		return new JLabel(icon);
+	}
+}
+
+class Song {
+
+	private int duration;
+	private String songAddress;
+	private AdvancedPlayer mp3Player;
+	private InputStream songStream;
+
+	/**
+	 * Songs can be constructed from files on your computer or Internet
+	 * addresses.
+	 * 
+	 * Examples: <code> 
+	 * 		new Song("everywhere.mp3"); 	//from default package 
+	 * 		new Song("/Users/joonspoon/music/Vampire Weekend - Modern Vampires of the City/03 Step.mp3"); 
+	 * 		new	Song("http://freedownloads.last.fm/download/569264057/Get%2BGot.mp3"); 
+	 * </code>
+	 */
+	public Song(String songAddress) {
+		this.songAddress = songAddress;
+	}
+
+	public void play() {
+		loadFile();
+		if (songStream != null) {
+			loadPlayer();
+			startSong();
+		} else
+			System.err.println("Unable to load file: " + songAddress);
+	}
+
+	public void setDuration(int seconds) {
+		this.duration = seconds * 100;
+	}
+
+	public void stop() {
+		if (mp3Player != null)
+			mp3Player.close();
+	}
+
+	private void startSong() {
+		Thread t = new Thread() {
+			public void run() {
+				try {
+					if (duration > 0)
+						mp3Player.play(duration);
+					else
+						mp3Player.play();
+				} catch (Exception e) {
+				}
+			}
+		};
+		t.start();
+	}
+
+	private void loadPlayer() {
+		try {
+			this.mp3Player = new AdvancedPlayer(songStream);
+		} catch (Exception e) {
+		}
+	}
+
+	private void loadFile() {
+		if (songAddress.contains("http"))
+			this.songStream = loadStreamFromInternet();
+		else
+			this.songStream = loadStreamFromComputer();
+	}
+
+	private InputStream loadStreamFromInternet() {
+		try {
+			return new URL(songAddress).openStream();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	private InputStream loadStreamFromComputer() {
+		try {
+			return new FileInputStream(songAddress);
+		} catch (FileNotFoundException e) {
+			return this.getClass().getResourceAsStream(songAddress);
+		}
+	}
+}
